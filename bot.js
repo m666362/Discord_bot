0d441b25bae3fc47b27468b39d74f483afd1386f
Md. Rayhan <55645384+m666362@users.noreply.github.com>
2022-05-22 12:17:27 +0600

    dm blocked for dm

--- bot.js
+++ bot.js
@@ -8,11 +8,31 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on("message", (msg) => {
-  const wasBotMentioned = msg?.mentions?.users?.get(client.user.id)? true:false;
+client.on("message",async (msg) => {
+  const wasBotMentioned = msg?.mentions?.users?.get(client.user.id)
+    ? true
+    : false;
   if (wasBotMentioned) {
     try {
       msg.reply("I am here for your service. How can I help you?");
+    } catch (error) {
+      console.warn("Failed to respond to mention.");
+      console.warn(error);
+    }
+  }
+
+  // Ignore bot message
+  if (msg?.author?.bot) {
+    return;
+  }
+
+  // Ignore any messages sent as direct messages.
+  // The bot will only accept commands issued in
+  // a guild.
+  if (!msg.channel.guild && msg.content!==undefined) {
+    try {
+      console.log(msg.content);
+      return await msg.reply("I was told not to reply personally");
     } catch (error) {
       console.warn("Failed to respond to mention.");
       console.warn(error);
