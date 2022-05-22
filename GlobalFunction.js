b052f9b5d1ab8348e3a33685a4d003d946ee8c67
Md. Rayhan <55645384+m666362@users.noreply.github.com>
2022-05-22 12:30:32 +0600

    missing command

--- GlobalFunction.js
+++ GlobalFunction.js
@@ -1,5 +1,32 @@
-const { newRole } = require("./Constant")
+const { newRole } = require("./Constant");
 
-module.exports.addUser = (member)=>{
-    member.roles.add(newRole)
-}
+const commandHandlerForCommandName = {};
+commandHandlerForCommandName["present"] = (msg) => {
+  try {
+    msg.reply("I am here for your service. How can I help you?");
+  } catch (error) {
+    console.warn("Failed to respond to mention.");
+    console.warn(error);
+  }
+};
+
+commandHandlerForCommandName["banDM"] = (msg) => {
+  try {
+    console.log(msg.content);
+    return msg.reply("I was told not to reply personally");
+  } catch (error) {
+    console.warn("Failed to respond to mention.");
+    console.warn(error);
+  }
+};
+
+commandHandlerForCommandName["commandNotMentioned"] = (msg) => {
+  try {
+    return msg.reply("Command missing. Please check documentation");
+  } catch (error) {
+    console.warn("Failed to respond to mention.");
+    console.warn(error);
+  }
+};
+
+module.exports = commandHandlerForCommandName;
--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Client } = require("discord.js");
 const { PREFIX, ModMe } = require("./Constant");
+const commandHandlerForCommandName = require("./GlobalFunction");
 const { addUser } = require("./GlobalFunction");
 const client = new Client();
 
@@ -8,39 +9,29 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on("message",async (msg) => {
+client.on("message", async (msg) => {
+  // Check bot is mentioned or not
   const wasBotMentioned = msg?.mentions?.users?.get(client.user.id)
     ? true
     : false;
   if (wasBotMentioned) {
-    try {
-      msg.reply("I am here for your service. How can I help you?");
-    } catch (error) {
-      console.warn("Failed to respond to mention.");
-      console.warn(error);
-    }
+    return commandHandlerForCommandName?.present(msg);
   }
 
-  // Ignore bot message
+  // Ignore bot's message
   if (msg?.author?.bot) {
     return;
   }
 
   // Ignore any messages sent as direct messages.
-  // The bot will only accept commands issued in
-  // a guild.
-  if (!msg.channel.guild && msg.content!==undefined) {
-    try {
-      console.log(msg.content);
-      return await msg.reply("I was told not to reply personally");
-    } catch (error) {
-      console.warn("Failed to respond to mention.");
-      console.warn(error);
-    }
+  // The bot will only accept commands issued in a guild.
+  if (!msg.channel.guild && msg.content !== undefined) {
+    return commandHandlerForCommandName?.banDM(msg);
   }
 
-  if (msg.content === `${PREFIX}${ModMe}`) {
-    addUser(msg.member);
+  // Ignore any message that doesn't start with the correct prefix.
+  if (!msg?.content?.startsWith(PREFIX)) {
+    return commandHandlerForCommandName?.commandNotMentioned(msg);
   }
 });
 
