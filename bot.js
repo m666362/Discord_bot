b832e11a302d2557b7b1e859aed1ddb23a42ad5c
Md. Rayhan <55645384+m666362@users.noreply.github.com>
2022-05-23 10:46:50 +0600

    deploy-commands.js

--- bot.js
+++ bot.js
@@ -1,14 +1,30 @@
 // Require the necessary discord.js classes
-require("dotenv").config()
-const { Client, Intents } = require('discord.js');
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log(`Logged in as ${client.user.username}!`);
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
 });
 
 // Login to Discord with your client's token
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.token);
